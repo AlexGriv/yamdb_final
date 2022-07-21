f1cd7410a76ca058ce193f3acc9650358d3ed823
AlexGriv <lksa-wd@yandex.ru>
2022-07-22 01:53:46 +1000

    isort_sucks_true4

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
 
-
 from api.views import (CommentViewSet, MyTokenObtainPairView, ReviewViewSet,
                        SignUpAPIView, UserSelfView, UserViewSet)
 
