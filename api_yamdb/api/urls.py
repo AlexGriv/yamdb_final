954aff09d2b2c55c40faa651cb8d6f18cd5bd3a5
AlexGriv <lksa-wd@yandex.ru>
2022-07-22 00:53:54 +1000

    pytest_ok3

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import (
-    CommentViewSet,
-    MyTokenObtainPairView,
-    ReviewViewSet,
-    SignUpAPIView,
-    UserSelfView,
-    UserViewSet
-)
+
+from api.views import (CommentViewSet, MyTokenObtainPairView, ReviewViewSet,
+                       SignUpAPIView, UserSelfView, UserViewSet)
 
 from .views import CategoriesViewSet, GenresViewSet, TitleViewSet
 
