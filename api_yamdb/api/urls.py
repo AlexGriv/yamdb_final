18a06418e8bc96887cda928e11143c3113f9662e
AlexGriv <lksa-wd@yandex.ru>
2022-07-22 01:28:41 +1000

    isort_sucks

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
+
 from api.views import (CommentViewSet, MyTokenObtainPairView, ReviewViewSet,
                        SignUpAPIView, UserSelfView, UserViewSet)
+
 from .views import CategoriesViewSet, GenresViewSet, TitleViewSet
 
 router_v1 = routers.DefaultRouter()
