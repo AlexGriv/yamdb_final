6edf666707c067009c71d332e2180c78c23080a6
AlexGriv <lksa-wd@yandex.ru>
2022-07-25 23:05:58 +1000

    secret_to_git

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,8 +8,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='asdas645675788333s')
 
-DB_ENGINE = 'django.db.backends.postgresql'
-
 DEBUG = False
 
 ALLOWED_HOSTS = [
