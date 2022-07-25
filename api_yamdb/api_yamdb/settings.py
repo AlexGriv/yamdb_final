f5f77b08ec81e36893f12641cad08cd19ff9471d
AlexGriv <lksa-wd@yandex.ru>
2022-07-25 22:19:18 +1000

    h2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,6 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='asdas645675788333s')
 
+DB_ENGINE = 'django.db.backends.postgresql'
+
 DEBUG = False
 
 ALLOWED_HOSTS = [
@@ -77,7 +79,7 @@ SIMPLE_JWT = {
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv(default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
